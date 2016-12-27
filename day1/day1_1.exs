94d1c12fde7c45f25dbd25895e52778ef665c8cd
pruser <pruser@users.noreply.github.com>
2016-12-27 21:28:16 +0100

    solution for part 1 and part 2

--- day1/day1_1.exs
+++ day1/day1_1.exs
@@ -1,6 +1,10 @@
 defmodule Day1 do
-  def process(steps) do
-    process({{0,0}, :up}, steps)
+  def process_part1(steps) do
+    process({:ok, {0,0}, :up, []}, steps, fn(_) -> false end)
+  end
+
+  def process_part2(steps) do
+    process({:ok, {0,0}, :up, []}, steps, &break_condition(&1))
   end
 
   defp read(str) do
@@ -10,41 +14,84 @@ defmodule Day1 do
     end
   end
 
-  defp process({point, orientation}, [head | tail]) do
+  defp process({:ok, _, _, _} = s, [head | tail], break_cond) do
       {d, n} = read(head)
-      transform(point, orientation, {d, n}) |> process(tail)
+      transform(s, d, n, break_cond) |> process(tail, break_cond)
   end
 
-  defp process({point, _}, []) do
-    point
+  defp process({:ok, _, _, _} = s, [], _) do
+    s
   end
 
-  defp transform({x, y}, :up, {dir, steps}) do
+  defp process({:break, _, _, _} = s, _, _) do
+    s
+  end
+
+  defp break_condition({_s, position, _ori, visited}) do
+    Enum.any?(visited |> Enum.drop(-1), fn(x) -> x == position end)  
+  end
+
+  defp transform({_, __pos, ori, _visited} = s, dir, steps, break_cond) do
+    new_ori = change_direction(ori, dir)
+    do_transform(s, new_ori, steps, break_cond)
+  end
+
+  defp do_transform({:ok, _, _, _} = s, _, 0, _) do
+    s
+  end
+
+  defp do_transform({:break, _, _, _} = s, _, _, _) do
+    s
+  end
+
+  defp do_transform({p, pos, _ori, visited}, new_ori, steps, break_cond) do
+    new_pos = move(pos, new_ori)
+    s = {p,  new_pos, new_ori, visited ++ [new_pos]}
+    if !break_cond.(s) do
+      do_transform(s, new_ori, steps-1, break_cond)
+    else 
+      {:break, new_pos, new_ori, visited ++ [new_pos]}
+    end 
+  end
+
+
+  defp change_direction(:up, dir) do
     case dir do
-      :r -> {{x, y+steps}, :right}
-      :l -> {{x, y-steps}, :left}
-    end
-  end
-
-  defp transform({x, y}, :down, {dir, steps}) do
-    case dir do
-      :r -> {{x, y-steps}, :left}
-      :l -> {{x, y+steps}, :right}
+      :r -> :right
+      :l -> :left
     end
   end
   
-  defp transform({x, y}, :left, {dir, steps}) do
+  defp change_direction(:down, dir) do
     case dir do
-      :r -> {{x+steps, y}, :up}
-      :l -> {{x-steps, y}, :down}
+      :r -> :left
+      :l -> :right
     end
   end
-  
-  defp transform({x, y}, :right, {dir, steps}) do
+
+  defp change_direction(:left, dir) do
     case dir do
-      :r -> {{x-steps, y}, :down}
-      :l -> {{x+steps, y}, :up}
+      :r -> :up
+      :l -> :down
     end
+  end
+
+  defp change_direction(:right, dir) do
+    case dir do
+      :r -> :down
+      :l -> :up
+    end
+  end
+
+  defp move({x,y}, :up), do: {x+1, y} 
+  defp move({x,y}, :down), do: {x-1, y} 
+  defp move({x,y}, :right), do: {x, y+1} 
+  defp move({x,y}, :left), do: {x, y-1} 
+end
+
+defimpl String.Chars, for: Tuple do 
+  def to_string({x, y}) do
+    "{" <> Kernel.to_string(x) <> ", " <> Kernel.to_string(y) <> "}"
   end
 end
 
@@ -54,8 +101,15 @@ if not File.exists?(fname) do
   raise "wrong file name"
 end
 
-distance = fn({x, y}) -> x+y end
+distance = fn({x,y}) -> abs(x)+abs(y) end
+
+get_result = fn({s, p, _, _}) -> {s, distance.(p)} end
+
+
 
 {:ok, data} = File.read(fname)
-data |> String.split([", ", "\n"], trim: true) |> Day1.process |> distance.() |> IO.puts
+steps = data |> String.split([", ", "\n"], trim: true) 
+
+steps |> Day1.process_part1 |> get_result.() |> IO.puts
+steps |> Day1.process_part2 |> get_result.() |> IO.puts
 
